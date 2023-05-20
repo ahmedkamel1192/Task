07dd32a02d9491d757cec0847944ebd3553a5e6b
ahmedkamel1192 <ahmed.kamel1192@gmail.com>
2023-05-20 19:28:48 +0300

    fix ui issue

--- resources/js/Pages/Tasks/Create.jsx
+++ resources/js/Pages/Tasks/Create.jsx
@@ -38,7 +38,7 @@ export default function Dashboards() {
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 bg-white border-b border-gray-200">
+                    <div className="p-6 bg-white border-b h-screen border-gray-200">
 
                         <div className="flex items-center justify-between mb-6">
                             <Link
@@ -71,7 +71,7 @@ export default function Dashboards() {
                                     <label className="">Description</label>
                                     <textarea
                                         type="text"
-                                        className="w-full rounded border"
+                                        className="w-full p-2 rounded border"
                                         label="description"
                                         name="description"
                                         errors={errors.body}
